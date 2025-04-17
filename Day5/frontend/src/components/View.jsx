7bca037f2559260b5237bd7e4a6f5a0efaf9d1dc
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-04-17 15:28:04 +0530

    Day5

--- Day5/frontend/src/components/View.jsx
+++ Day5/frontend/src/components/View.jsx
@@ -1,37 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 const View = () => {
-    const [users,setUsers]=useState([])
-    const handleview=async () => {
-        const res=await axios.get('https://fsdbackenddsc.onrender.com/users')
-        setUsers(res.data)
+    const [users, setUsers] = useState([]);
+    const handleview=async ()=>{
+       const res=await axios.get('http://localhost:9000/users');
+       setUsers(res.data);
     }
     useEffect(()=>{
         handleview();
-    },[])
+     },[])
   return (
     <div>
-      <h1>View Registered Users</h1>
-      <table>
-        <thead>
-            <tr>
-                <td>Id</td>
-                <td>Name</td>
-                <td>Age</td>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                users.map(user =>(
-                <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.age}</td>
+        <h1>View Registered Users</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Age</th>
                 </tr>
-                ))
-           }
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+                {
+                    users.map((user)=>(
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.age}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+      
     </div>
   )
 }
